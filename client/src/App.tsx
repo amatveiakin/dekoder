8fb67e9bd3bac02ea0de375432816705312f69df
Andrei Matveiakin <a.matveiakin@gmail.com>
2023-04-24 17:23:35 +0400

    MUI UI stub

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,45 @@
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";  // TODO: add favicon
 import "./App.css";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Card,
+  Paper,
+  Stack,
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import MailOutlinedIcon from "@mui/icons-material/MailOutlined";
 
 function App() {
   return (
-    <div className="App">
+    <Box>
       <CssBaseline />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Stack spacing={2}>
+        <Card>Card 1</Card>
+        <Card>Card 2</Card>
+      </Stack>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+        //   showLabels
+        //   value={value}
+        //   onChange={(event, newValue) => {
+        //     setValue(newValue);
+        //   }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <BottomNavigationAction
+            label="Our rounds"
+            icon={<MailOutlinedIcon />}
+          />
+          <BottomNavigationAction label="Their rounds" icon={<MailIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </Box>
   );
 }
 
