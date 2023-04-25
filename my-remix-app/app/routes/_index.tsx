6e8ee3f66307e125c854ad12731e8cdc34e85609
Andrei Matveiakin <a.matveiakin@gmail.com>
2023-04-25 22:27:57 +0400

    Join game UI

--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -1,9 +1,31 @@
-import { Container } from "@mui/material";
+import { useNavigate } from "@remix-run/react";
+import { Box, Button, Stack, Typography } from "@mui/material";
 
 export default function Index() {
+  const navigate = useNavigate();
+
   return (
-    <Container>
-      <Container sx={{ pt: 2, pb: 2 }}>Welcome!</Container>
-    </Container>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Stack spacing={2} sx={{ width: 280 }}>
+        <Typography variant="h6" align="center" sx={{ mb: 1 }}>
+          Welcome to decoder online!
+        </Typography>
+        <Button variant="contained" size="large" onClick={() => {} /*TODO*/}>
+          New game
+        </Button>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => navigate("/join")}
+        >
+          Join game
+        </Button>
+      </Stack>
+    </Box>
   );
 }
---
+++ my-remix-app/app/routes/join.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useNavigate } from "@remix-run/react";
+import { Box, Button, Stack, TextField, ButtonGroup } from "@mui/material";
+
+// TODO: Change color theme so that buttons are neither blue nor red.
+export default function Join() {
+  const [team, setTeam] = useState("red");
+  const [gameId, setGameId] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      {/* `useFlexGap` required to make child margins work */}
+      <Stack useFlexGap spacing={2} sx={{ width: 280 }}>
+        {/* TODO: Auto capitalize */}
+        {/* TODO: Increase input test size; center input */}
+        <TextField
+          variant="outlined"
+          fullWidth
+          type="text"
+          required
+          autoComplete="off"
+          name="gameId"
+          label="Game ID"
+          value={gameId}
+          onChange={({ target }) => setGameId(target.value)}
+        />
+        <ButtonGroup variant="outlined">
+          <Button
+            variant={team === "red" ? "contained" : "outlined"}
+            onClick={() => setTeam("red")}
+            fullWidth
+          >
+            Team Red
+          </Button>
+          <Button
+            variant={team === "blue" ? "contained" : "outlined"}
+            onClick={() => setTeam("blue")}
+            fullWidth
+          >
+            Team Blue
+          </Button>
+        </ButtonGroup>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ mt: 2 }}
+          onClick={() => {
+            navigate(`/game/${gameId}/${team}`, { replace: true });
+          }}
+        >
+          Join as {team}!
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
